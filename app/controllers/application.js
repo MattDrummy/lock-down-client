8a150b7ecf64b2ff4cac945c79ba87464e815fbd
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-20 16:54:24 -0600

    styled header

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  loggedIn: true,
+  user: "GUEST",
+  actions: {
+    logIn(){
+      this.set('loggedIn', true);
+    },
+    logOut(){
+      this.set('loggedIn', false);
+    },
+    signUp(){
+
+    },
+  }
 });
