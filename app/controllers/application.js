c68cc89ee09943f342b1e0c2019e88642537681a
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-21 19:31:33 -0600

    removed center block and added modal for log in, finished styling of game area

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,19 +2,30 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   loggedIn: false,
-  user: "GUEST",
+  user: "",
+  logInUsername: "",
+  logInPassword: "",
   signUpUsername: "",
   signUpEmail: "",
   signUpPassword: "",
   actions: {
     logIn(){
+      this.set('user', this.get('logInUsername'))
+      this.set('logInPassword', "")
+      this.set('logInUsername', "")
       this.set('loggedIn', true);
     },
     logOut(){
       this.set('loggedIn', false);
+      this.set('user', "GUEST")
     },
     signUp(){
       this.set("user", this.get("signUpUsername"))
+      this.set('signUpUsername', '');
+      this.set('signUpEmail', '');
+      this.set('signUpPassword', '')
+      this.set('loggedIn', true);
+
     }
   }
 });
