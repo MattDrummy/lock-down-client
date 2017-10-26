26ee63bd1a22da72afd250d79b296b1f85f14814
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-26 16:22:09 -0600

    updated modals to use bootstrap forms and validation

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  createGameRole: "operator",
+  createGamePassword: "",
+  createGameEmail: "",
   actions: {
     createGame(modal){
       modal.close();
     },
     closeModal(modal){
+      this.set('createGameRole', "")
+      this.set('createGameEmail', "")
+      this.set('createGamePassword', "")
       modal.close()
     }
   }
