7e2bb0a0a84fd21e57388f414f64fb4e725cfd7d
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-29 15:58:22 -0600

    clean up code

--- app/components/game-area.js
+++ app/components/game-area.js
@@ -16,49 +16,53 @@ export default Ember.Component.extend({
     })
   },
   init(){
-    this._super(...arguments);
-    this.set('gameChatMessages', [])
-    this.set('consoleMessages', [])
-    const socket = this.get('socketIOService').socketFor(this.get('url'))
-    const gameChat = this.get('gameChatMessages');
-    let user = this.get('user');
-    let room = this.get('room');
+    let c = this
+    console.log(c);
+    c._super(...arguments);
+    c.set('gameChatMessages', [])
+    c.set('consoleMessages', [])
+    const socket = c.get('socketIOService').socketFor(c.get('url'))
+    const gameChat = c.get('gameChatMessages');
+    let user = c.get('user');
+    let room = c.get('room');
     socket.emit('open', [user,room]);
     socket.on('close', (message)=>{
       gameChat.pushObject(message)
-      this.get('fixScroll')('chat-window')
+      c.get('fixScroll')('chat-window')
 
     });
     socket.on('message', (message)=>{
       gameChat.pushObject(message)
-      this.get('fixScroll')('chat-window')
+      c.get('fixScroll')('chat-window')
 
     });
   },
   willDestroyElement(){
-    this._super(...arguments);
-    let url = this.get('url');
-    let user = this.get('user');
-    let room = this.get('room');
-    const socket = this.get('socketIOService').socketFor(url);
+    let c = this
+    c._super(...arguments);
+    let url = c.get('url');
+    let user = c.get('user');
+    let room = c.get('room');
+    const socket = c.get('socketIOService').socketFor(url);
     socket.emit('close', [user,room]);
-    this.set('gameChatMessages', []);
-    this.get('socketIOService').closeSocketFor(url)
+    c.set('gameChatMessages', []);
+    c.get('socketIOService').closeSocketFor(url)
   },
   actions: {
     consoleEnter(){
-      let command = this.get('consoleInput').split(' ')[0];
-      let option = this.get('consoleInput').split(' ')[1];
-      let optionParams = this.get('consoleInput').split(' ').slice(2)
-      let role = this.get('role');
-      let currPath = this.get('currNode').path;
-      let readOut = this.get('consoleMessages');
-      let commandList = this.get(`${role}Commands`);
-      let fileStructure = this.get(`${role}FileStructure`);
-      let operatorPassword = this.get(`operatorPassword`);
-      let port = this.get(`${role}Port`);
-      let currLocation = this.get(`${role}Location`);
-      let room = this.get('room');
+      let c = this;
+      let command = c.get('consoleInput').split(' ')[0];
+      let option = c.get('consoleInput').split(' ')[1];
+      let optionParams = c.get('consoleInput').split(' ').slice(2)
+      let role = c.get('role');
+      let currPath = c.get('currNode').path;
+      let readOut = c.get('consoleMessages');
+      let commandList = c.get(`${role}Commands`);
+      let fileStructure = c.get(`${role}FileStructure`);
+      let operatorPassword = c.get(`operatorPassword`);
+      let port = c.get(`${role}Port`);
+      let currLocation = c.get(`${role}Location`);
+      let room = c.get('room');
       let data = {
         commandList,
         fileStructure,
@@ -72,38 +76,39 @@ export default Ember.Component.extend({
         currLocation,
       }
       if (command == "change") {
-        this.set('role', role == "operator" ? "operative" : "operator");
-        readOut.pushObject(`${currPath} ${this.get('consoleInput')}`)
-        readOut.pushObject(`Changed role to ${this.get('role')}`);
+        c.set('role', role == "operator" ? "operative" : "operator");
+        readOut.pushObject(`${currPath} ${c.get('consoleInput')}`)
+        readOut.pushObject(`Changed role to ${c.get('role')}`);
       } else {
         let operation = commandList.filter((e)=>{
           return e.command == command
         })[0]
         if (operation) {
           if (operation.run(data)) {
-            const socket = this.get('socketIOService').socketFor(this.get('url'))
+            const socket = c.get('socketIOService').socketFor(c.get('url'))
             socket.emit("message", [room, `YOU WIN!`])
-            this.set('chatInput', '')
-            this.get('fixScroll')('chat-window')
+            c.set('chatInput', '')
+            c.get('fixScroll')('chat-window')
           }
         } else {
-          readOut.pushObject(`${currPath} ${this.get('consoleInput')}`)
+          readOut.pushObject(`${currPath} ${c.get('consoleInput')}`)
           readOut.pushObject("no such command exists")
         }
       }
       readOut.pushObject(currPath)
-      this.set('consoleInput', '')
-      this.get('fixScroll')('console-window')
+      c.set('consoleInput', '')
+      c.get('fixScroll')('console-window')
 
     },
     chatEnter(){
-      const socket = this.get('socketIOService').socketFor(this.get('url'))
-      let room = this.get('room');
-      let user = this.get('user');
-      let chatInput = this.get('chatInput')
+      let c = this
+      const socket = c.get('socketIOService').socketFor(c.get('url'))
+      let room = c.get('room');
+      let user = c.get('user');
+      let chatInput = c.get('chatInput')
       socket.emit("message", [room, `${user}: ${chatInput}`])
-      this.set('chatInput', '')
-      this.get('fixScroll')('chat-window')
+      c.set('chatInput', '')
+      c.get('fixScroll')('chat-window')
     },
   },
 });
