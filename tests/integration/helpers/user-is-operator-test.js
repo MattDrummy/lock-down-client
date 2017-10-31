4c64a29c5f97b3181be3fd6a6e7bdce29a5523f9
Matthew Drumheller <MattDrummy@gmail.com>
2017-10-31 10:24:49 -0600

    added helper to help better explain instructions

---
+++ tests/integration/helpers/user-is-operator-test.js
@@ -0,0 +1,17 @@
+
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('user-is-operator', 'helper:user-is-operator', {
+  integration: true
+});
+
+// Replace this with your real tests.
+test('it renders', function(assert) {
+  this.set('inputValue', '1234');
+
+  this.render(hbs`{{user-is-operator inputValue}}`);
+
+  assert.equal(this.$().text().trim(), '1234');
+});
+
